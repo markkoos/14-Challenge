76d08141170c9f2c590029aeb4c0fa82d2dde7c9
markkoos <markusimercado@gmail.com>
2022-10-25 16:54:58 -0700

    boilerplate sequelize connection with handlebar landing page

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require(`express`);
+const exphbs = require(`express-handlebars`);
+const routes = require('./controllers');
+// const helpers = require(`./utils/helpers`);
+
+// import sequelize connection
+const sequelize = require(`./config/connection`);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+// create template engine
+const hbs = exphbs.create({});
+
+app.engine(`handlebars`, hbs.engine);
+app.set(`view engine`, `handlebars`);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(`public`));
+// app.use(routes);
+
+app.get(`/`, (req, res) => {
+    res.render(`home`);
+});
+
+app.get(`*`, (req, res) => {
+    res.render(`home`);
+})
+
+sequelize.sync({force: false}).then(() => {
+    app.listen(PORT, () => {
+        console.log(`App listening on port ${PORT}!`)
+    })
+})
