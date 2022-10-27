2250fd20e18d3a8309c1f27372aef72a89a01c8f
markkoos <markusimercado@gmail.com>
2022-10-26 22:56:20 -0700

    started on handlebar page styling with bootstrap

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require(`express`);
 const exphbs = require(`express-handlebars`);
 const routes = require('./controllers');
+const session = require(`express-session`);
 // const helpers = require(`./utils/helpers`);
 
 // import sequelize connection
